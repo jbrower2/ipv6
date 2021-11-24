3d591e15b548e24ca265fb371e7501d33ed7e3e4
Jeff Brower <jeff@jeffbrower.com>
2021-11-24 08:44:42 -0500

    Initial commit
    * Add tests for unimplemented methods

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Converts an IPv6 address to segments.
+ *
+ * This method will throw an error on invalid input.
+ * See [Wikipedia](https://en.wikipedia.org/wiki/IPv6_address) for more information on IPv6 address formats.
+ *
+ * @param address The IPv6 string.
+ * @returns The expanded format of the address.
+ */
+export function toSegments(address: string): number[] {
+	console.log(address);
+	return [];
+}
+
+/**
+ * Formats an IPv6 array as a minimal string.
+ *
+ * @param segments The IPv6 address segments.
+ * @returns The minimal form of the address.
+ */
+export function toString(segments: number[]): string {
+	console.log(segments);
+	return "";
+}
