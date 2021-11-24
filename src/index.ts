5e6586a5bc656d143bdc53946036648102687def
Jeff Brower <jeff@jeffbrower.com>
2021-11-24 09:18:31 -0500

    Implement toSegments and add negative tests

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,52 @@
  * @returns The expanded format of the address.
  */
 export function toSegments(address: string): number[] {
-	console.log(address);
-	return [];
+	const sections = address.split("::");
+	if (sections.length > 2) {
+		throw new Error(
+			`IPv6 address should only have one zero expansion: ${address}`
+		);
+	}
+
+	/** All parts of the address, in original string form, `undefined` if an expanded zero. */
+	const strings: (string | undefined)[] = [];
+
+	if (sections.length === 1) {
+		const parts = sections[0].split(":");
+		if (parts.length !== 8) {
+			throw new Error(`IPv6 address should have eight parts: ${address}`);
+		}
+
+		strings.push(...parts);
+	} else {
+		// '::' found, so we need to expand zeros
+
+		const start = !sections[0] ? [] : sections[0].split(":");
+		const end = !sections[1] ? [] : sections[1].split(":");
+
+		// 6 because zero expansion must account for at least two slots
+		if (start.length + end.length > 6) {
+			throw new Error(
+				`IPv6 address can only have zero expansion for two or more zeros: ${address}`
+			);
+		}
+
+		// push some number of `undefined` values between the start and end arrays
+		strings.push(...start, ...Array(8 - start.length - end.length), ...end);
+	}
+
+	return strings.map((s) => {
+		if (!s) {
+			return 0;
+		}
+
+		if (!/^[0-9a-f]{1,4}$/i.test(s)) {
+			throw new Error(`IPv6 segments must be 1-4 hexadecimal digits: ${s}`);
+		}
+
+		// parse from hex
+		return parseInt(s, 16);
+	});
 }
 
 /**
--- test/index.test.js
+++ test/index.test.js
@@ -3,43 +3,97 @@ const { toSegments, toString } = require("../dist");
 // toSegments
 
 test('toSegments("::")', () => {
-	expect(toSegments("::")).toBe([0, 0, 0, 0, 0, 0, 0, 0]);
+	expect(toSegments("::")).toStrictEqual([0, 0, 0, 0, 0, 0, 0, 0]);
 });
 
 test('toSegments("0000:0000:0000:0000:0000:0000:0000:0000")', () => {
-	expect(toSegments("0000:0000:0000:0000:0000:0000:0000:0000")).toBe([
+	expect(toSegments("0000:0000:0000:0000:0000:0000:0000:0000")).toStrictEqual([
 		0, 0, 0, 0, 0, 0, 0, 0,
 	]);
 });
 
 test('toSegments("0001::0002")', () => {
-	expect(toSegments("0001::0002")).toBe([1, 0, 0, 0, 0, 0, 0, 2]);
+	expect(toSegments("0001::0002")).toStrictEqual([1, 0, 0, 0, 0, 0, 0, 2]);
 });
 
 test('toSegments("0001:0000:0000:0000:0000:0000:0000:0002")', () => {
-	expect(toSegments("0001:0000:0000:0000:0000:0000:0000:0002")).toBe([
+	expect(toSegments("0001:0000:0000:0000:0000:0000:0000:0002")).toStrictEqual([
 		1, 0, 0, 0, 0, 0, 0, 2,
 	]);
 });
 
 test('toSegments("0001:0002:0003:0004:0005:0006:0007:0008")', () => {
-	expect(toSegments("0001:0002:0003:0004:0005:0006:0007:0008")).toBe([
+	expect(toSegments("0001:0002:0003:0004:0005:0006:0007:0008")).toStrictEqual([
 		1, 2, 3, 4, 5, 6, 7, 8,
 	]);
 });
 
 test('toSegments("1:2:3:4:5:6:7:8")', () => {
-	expect(toSegments("1:2:3:4:5:6:7:8")).toBe([1, 2, 3, 4, 5, 6, 7, 8]);
+	expect(toSegments("1:2:3:4:5:6:7:8")).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8]);
 });
 
 test('toSegments("2601:0600:897f:e4a9:1cb3:5011:0cfe:45c4")', () => {
-	expect(toSegments("2601:0600:897f:e4a9:1cb3:5011:0cfe:45c4")).toBe([
+	expect(toSegments("2601:0600:897f:e4a9:1cb3:5011:0cfe:45c4")).toStrictEqual([
 		9729, 1536, 35199, 58537, 7347, 20497, 3326, 17860,
 	]);
 });
 
 test('toSegments("2601::1:45c4")', () => {
-	expect(toSegments("2601::1:45c4")).toBe([9729, 0, 0, 0, 0, 0, 1, 17860]);
+	expect(toSegments("2601::1:45c4")).toStrictEqual([
+		9729, 0, 0, 0, 0, 0, 1, 17860,
+	]);
+});
+
+test('toSegments("1:2:3::4:5:6")', () => {
+	expect(toSegments("1:2:3::4:5:6")).toStrictEqual([1, 2, 3, 0, 0, 4, 5, 6]);
+});
+
+test('toSegments("1:23:456:7890::")', () => {
+	expect(toSegments("1:23:456:7890::")).toStrictEqual([
+		1, 35, 1110, 30864, 0, 0, 0, 0,
+	]);
+});
+
+test('toSegments("::1:23:456:7890")', () => {
+	expect(toSegments("::1:23:456:7890")).toStrictEqual([
+		0, 0, 0, 0, 1, 35, 1110, 30864,
+	]);
+});
+
+test('toSegments("1::2::3")', () => {
+	expect(() => toSegments("1::2::3")).toThrow(
+		/^IPv6 address should only have one zero expansion:/
+	);
+});
+
+test('toSegments("")', () => {
+	expect(() => toSegments("")).toThrow(
+		/^IPv6 address should have eight parts:/
+	);
+});
+
+test('toSegments("1")', () => {
+	expect(() => toSegments("1")).toThrow(
+		/^IPv6 address should have eight parts:/
+	);
+});
+
+test('toSegments("1:2:3:4:5:6:7:8:9")', () => {
+	expect(() => toSegments("1:2:3:4:5:6:7:8:9")).toThrow(
+		/^IPv6 address should have eight parts:/
+	);
+});
+
+test('toSegments("1:2:3::4:5:6:7")', () => {
+	expect(() => toSegments("1:2:3::4:5:6:7")).toThrow(
+		/^IPv6 address can only have zero expansion for two or more zeros:/
+	);
+});
+
+test('toSegments("1:23:456::abcde")', () => {
+	expect(() => toSegments("1:23:456::abcde")).toThrow(
+		/^IPv6 segments must be 1-4 hexadecimal digits:/
+	);
 });
 
 // toString
